ede7acf4d9675c135e42a460ad1dcace2c1a07d3
Czy2019 <63694486+Czy2019@users.noreply.github.com>
2024-02-13 11:15:06 +0800

    Add files via upload

--- generated/offsets.cs
+++ generated/offsets.cs
@@ -1,33 +1,33 @@
 /*
  * Created using https://github.com/a2x/cs2-dumper
- * Fri, 9 Feb 2024 02:38:11 +0000
+ * Tue, 13 Feb 2024 02:41:15 +0000
  */
 
 public static class client_dll { // client.dll
-    public const nint dwEntityList = 0x18B0FC8;
-    public const nint dwForceAttack = 0x171F160;
-    public const nint dwForceAttack2 = 0x171F1F0;
-    public const nint dwForceBackward = 0x171F430;
-    public const nint dwForceCrouch = 0x171F700;
-    public const nint dwForceForward = 0x171F3A0;
-    public const nint dwForceJump = 0x171F670;
-    public const nint dwForceLeft = 0x171F4C0;
-    public const nint dwForceRight = 0x171F550;
-    public const nint dwGameEntitySystem = 0x19CC7D0;
+    public const nint dwEntityList = 0x18B1FE8;
+    public const nint dwForceAttack = 0x1720160;
+    public const nint dwForceAttack2 = 0x17201F0;
+    public const nint dwForceBackward = 0x1720430;
+    public const nint dwForceCrouch = 0x1720700;
+    public const nint dwForceForward = 0x17203A0;
+    public const nint dwForceJump = 0x1720670;
+    public const nint dwForceLeft = 0x17204C0;
+    public const nint dwForceRight = 0x1720550;
+    public const nint dwGameEntitySystem = 0x19CD800;
     public const nint dwGameEntitySystem_getHighestEntityIndex = 0x1510;
-    public const nint dwGameRules = 0x190DFA0;
-    public const nint dwGlobalVars = 0x1719E40;
-    public const nint dwGlowManager = 0x190E3B8;
-    public const nint dwInterfaceLinkList = 0x19FD7F8;
-    public const nint dwLocalPlayerController = 0x1900928;
-    public const nint dwLocalPlayerPawn = 0x17262E8;
-    public const nint dwPlantedC4 = 0x19161C8;
-    public const nint dwPrediction = 0x17261B0;
-    public const nint dwSensitivity = 0x190ECD8;
+    public const nint dwGameRules = 0x190EFC0;
+    public const nint dwGlobalVars = 0x171AE40;
+    public const nint dwGlowManager = 0x190F3D8;
+    public const nint dwInterfaceLinkList = 0x19FE828;
+    public const nint dwLocalPlayerController = 0x1901948;
+    public const nint dwLocalPlayerPawn = 0x17272E8;
+    public const nint dwPlantedC4 = 0x19171E8;
+    public const nint dwPrediction = 0x17271B0;
+    public const nint dwSensitivity = 0x190FCF8;
     public const nint dwSensitivity_sensitivity = 0x40;
-    public const nint dwViewAngles = 0x191E0E0;
-    public const nint dwViewMatrix = 0x19102B0;
-    public const nint dwViewRender = 0x1910B30;
+    public const nint dwViewAngles = 0x191F100;
+    public const nint dwViewMatrix = 0x19112D0;
+    public const nint dwViewRender = 0x1911B50;
 }
 
 public static class engine2_dll { // engine2.dll
@@ -36,12 +36,12 @@ public static class engine2_dll { // engine2.dll
     public const nint dwNetworkGameClient_getLocalPlayer = 0xF0;
     public const nint dwNetworkGameClient_maxClients = 0x250;
     public const nint dwNetworkGameClient_signOnState = 0x240;
-    public const nint dwWindowHeight = 0x5C99FC;
-    public const nint dwWindowWidth = 0x5C99F8;
+    public const nint dwWindowHeight = 0x5C9A2C;
+    public const nint dwWindowWidth = 0x5C9A28;
 }
 
 public static class game_info { // Some additional information about the game at dump time
-    public const nint buildNumber = 0x36A6; // Game build number
+    public const nint buildNumber = 0x36A7; // Game build number
 }
 
 public static class inputsystem_dll { // inputsystem.dll
--- generated/offsets.py
+++ generated/offsets.py
@@ -1,33 +1,33 @@
 '''
 Created using https://github.com/a2x/cs2-dumper
-Fri, 9 Feb 2024 02:38:11 +0000
+Tue, 13 Feb 2024 02:41:15 +0000
 '''
 
 class client_dll: # client.dll
-    dwEntityList = 0x18B0FC8
-    dwForceAttack = 0x171F160
-    dwForceAttack2 = 0x171F1F0
-    dwForceBackward = 0x171F430
-    dwForceCrouch = 0x171F700
-    dwForceForward = 0x171F3A0
-    dwForceJump = 0x171F670
-    dwForceLeft = 0x171F4C0
-    dwForceRight = 0x171F550
-    dwGameEntitySystem = 0x19CC7D0
+    dwEntityList = 0x18B1FE8
+    dwForceAttack = 0x1720160
+    dwForceAttack2 = 0x17201F0
+    dwForceBackward = 0x1720430
+    dwForceCrouch = 0x1720700
+    dwForceForward = 0x17203A0
+    dwForceJump = 0x1720670
+    dwForceLeft = 0x17204C0
+    dwForceRight = 0x1720550
+    dwGameEntitySystem = 0x19CD800
     dwGameEntitySystem_getHighestEntityIndex = 0x1510
-    dwGameRules = 0x190DFA0
-    dwGlobalVars = 0x1719E40
-    dwGlowManager = 0x190E3B8
-    dwInterfaceLinkList = 0x19FD7F8
-    dwLocalPlayerController = 0x1900928
-    dwLocalPlayerPawn = 0x17262E8
-    dwPlantedC4 = 0x19161C8
-    dwPrediction = 0x17261B0
-    dwSensitivity = 0x190ECD8
+    dwGameRules = 0x190EFC0
+    dwGlobalVars = 0x171AE40
+    dwGlowManager = 0x190F3D8
+    dwInterfaceLinkList = 0x19FE828
+    dwLocalPlayerController = 0x1901948
+    dwLocalPlayerPawn = 0x17272E8
+    dwPlantedC4 = 0x19171E8
+    dwPrediction = 0x17271B0
+    dwSensitivity = 0x190FCF8
     dwSensitivity_sensitivity = 0x40
-    dwViewAngles = 0x191E0E0
-    dwViewMatrix = 0x19102B0
-    dwViewRender = 0x1910B30
+    dwViewAngles = 0x191F100
+    dwViewMatrix = 0x19112D0
+    dwViewRender = 0x1911B50
 
 class engine2_dll: # engine2.dll
     dwBuildNumber = 0x512554
@@ -35,11 +35,11 @@ class engine2_dll: # engine2.dll
     dwNetworkGameClient_getLocalPlayer = 0xF0
     dwNetworkGameClient_maxClients = 0x250
     dwNetworkGameClient_signOnState = 0x240
-    dwWindowHeight = 0x5C99FC
-    dwWindowWidth = 0x5C99F8
+    dwWindowHeight = 0x5C9A2C
+    dwWindowWidth = 0x5C9A28
 
 class game_info: # Some additional information about the game at dump time
-    buildNumber = 0x36A6 # Game build number
+    buildNumber = 0x36A7 # Game build number
 
 class inputsystem_dll: # inputsystem.dll
     dwInputSystem = 0x367A0
