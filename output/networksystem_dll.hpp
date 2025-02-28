fe44146d54d207e2861ff8a4d5abbb2f69eee78c
Leilani A. <168607226+kaleohanopahala@users.noreply.github.com>
2025-02-27 21:50:17 -0300

    Add files via upload

---
+++ output/networksystem_dll.hpp
@@ -0,0 +1,21 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-02-28 00:46:50.110041 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: networksystem.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace networksystem_dll {
+            // Parent: None
+            // Field count: 1
+            namespace ChangeAccessorFieldPathIndex_t {
+                constexpr std::ptrdiff_t m_Value = 0x0; // int32
+            }
+        }
+    }
+}
