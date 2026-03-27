c2636cece8735b65522f8162a298956f20fdd380
cacamelio <andreicov2006@hotmail.com>
2026-03-27 21:40:51 +0200

    latest game update

--- output/client_dll.cs
+++ output/client_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-19 10:05:24.652271200 UTC
+// 2026-03-27 19:40:07.337128 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: client.dll
