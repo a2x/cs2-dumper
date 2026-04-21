6a2624f4502d0de3a2a86f535f3c6f4b930f17f1
igq1o310 <ivan.grgec123grgec@gmail.com>
2026-04-21 20:29:04 +0200

    Add files via upload

---
+++ output/networksystem_dll.rs
@@ -0,0 +1,19 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-04-21 18:24:51.331450100 UTC
+
+#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
+
+pub mod cs2_dumper {
+    pub mod schemas {
+        // Module: networksystem.dll
+        // Class count: 1
+        // Enum count: 0
+        pub mod networksystem_dll {
+            // Parent: None
+            // Field count: 1
+            pub mod ChangeAccessorFieldPathIndex_t {
+                pub const m_Value: usize = 0x0; // int32
+            }
+        }
+    }
+}
---
+++ output/networksystem_dll.zig
@@ -0,0 +1,17 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2026-04-21 18:24:51.331450100 UTC
+
+pub const cs2_dumper = struct {
+    pub const schemas = struct {
+        // Module: networksystem.dll
+        // Class count: 1
+        // Enum count: 0
+        pub const networksystem_dll = struct {
+            // Parent: None
+            // Field count: 1
+            pub const ChangeAccessorFieldPathIndex_t = struct {
+                pub const m_Value: usize = 0x0; // int32
+            };
+        };
+    };
+};
