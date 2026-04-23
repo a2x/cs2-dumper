95ef2f5140551db751a993e7aeec4ae9c24ff9e3
KEV0143 <KEV0143@users.noreply.github.com>
2026-04-23 04:30:50 +0300

    Game update 1.41.5.4 (797)

--- output/scenesystem_dll.cs
+++ output/scenesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-22 23:25:08.898383200 UTC
+// 2026-04-23 01:25:07.655854600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: scenesystem.dll
--- output/schemasystem_dll.rs
+++ output/schemasystem_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-22 23:25:08.898383200 UTC
+// 2026-04-23 01:25:07.655854600 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
