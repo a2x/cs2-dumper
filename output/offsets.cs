5adcbcd5d842e35037c779a492252b7a47bd7ec1
killeromg003 <killeromg003@gmail.com>
2026-07-09 11:27:02 +0800

    update offset.cs

--- output/offsets.cs
+++ output/offsets.cs
@@ -1,27 +1,28 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-01 03:08:20.812145800 UTC
+// Updated with new offsets
 
 namespace CS2Dumper.Offsets {
     // Module: client.dll
     public static class ClientDll {
-        public const nint dwCSGOInput = 0x23560C0;
-        public const nint dwEntityList = 0x24E7680;
+        public const nint dwCSGOInput = 0x239E7F8;              
+        public const nint dwEntityList = 0x254DE50;             
         public const nint dwGameEntitySystem = 0x24E7680;
         public const nint dwGameEntitySystem_highestEntityIndex = 0x2090;
         public const nint dwGameRules = 0x2340FE8;
-        public const nint dwGlobalVars = 0x20616D0;
+        public const nint dwGlobalVars = 0x208ED60;             
         public const nint dwGlowManager = 0x233DDE0;
-        public const nint dwLocalPlayerController = 0x2320570;
-        public const nint dwLocalPlayerPawn = 0x2341528;
-        public const nint dwPlantedC4 = 0x234FE28;
+        public const nint dwLocalPlayerController = 0x202B350;  
+        public const nint dwLocalPlayerPawn = 0x21CFDF0;        
+        public const nint dwPlantedC4 = 0x23BA4F8;              
         public const nint dwPrediction = 0x2341430;
-        public const nint dwSensitivity = 0x233E8F8;
-        public const nint dwSensitivity_sensitivity = 0x58;
-        public const nint dwViewAngles = 0x2356748;
-        public const nint dwViewMatrix = 0x23469C0;
+        public const nint dwSensitivity = 0x239E7F8;            
+        public const nint dwSensitivity_sensitivity = 0x81C;    
+        public const nint dwViewAngles = 0x25C7630;             
+        public const nint dwViewMatrix = 0x23A8340;             
         public const nint dwViewRender = 0x2346D70;
         public const nint dwWeaponC4 = 0x22BED18;
     }
+
     // Module: engine2.dll
     public static class Engine2Dll {
         public const nint dwBuildNumber = 0x60CC74;
@@ -36,17 +37,56 @@ namespace CS2Dumper.Offsets {
         public const nint dwWindowHeight = 0x90E5C4;
         public const nint dwWindowWidth = 0x90E5C0;
     }
+
     // Module: inputsystem.dll
     public static class InputsystemDll {
         public const nint dwInputSystem = 0x42B50;
     }
+
     // Module: matchmaking.dll
     public static class MatchmakingDll {
         public const nint dwGameTypes = 0x1B0F80;
     }
+
     // Module: soundsystem.dll
     public static class SoundsystemDll {
         public const nint dwSoundSystem = 0x512360;
         public const nint dwSoundSystem_engineViewData = 0x7C;
+    }
+
+    // Class Fields (Structure-relative offsets)
+    public static class ClassFields {
+        public const nint m_iHealth = 0x348;
+        public const nint m_iTeamNum = 0x3E7;
+        public const nint m_lifeState = 0x354;
+        public const nint m_fFlags = 0x63;
+        public const nint m_pGameSceneNode = 0x330;
+        public const nint m_vecAbsVelocity = 0x3F8;
+        public const nint m_vecViewOffset = 0xE78;
+        public const nint m_clrRender = 0xC98;
+        public const nint m_vOldOrigin = 0x13B8;
+        public const nint m_bDormant = 0x103;
+        public const nint m_modelState = 0x140;
+        public const nint m_hPawn = 0x6BC;
+        public const nint m_iszPlayerName = 0x6F4;
+        public const nint m_vecCsViewPunchAngle = 0x48;
+        public const nint m_iShotsFired = 0x1C84;
+        public const nint m_pAimPunchServices = 0x14B8;
+        public const nint m_entitySpottedState = 0x1C58;
+        public const nint m_iIDEntIndex = 0x3DA;
+        public const nint m_aimPunchAngle = 0x58;
+        public const nint m_aimPunchCache = 0x68;
+        public const nint m_pClippingWeapon = 0x1610;
+        public const nint m_bIsScoped = 0x1C70;
+        public const nint m_flFlashDuration = 0x1428;
+        public const nint m_AttributeManager = 0x1200;
+        public const nint m_Item = 0x50;
+        public const nint m_iItemDefinitionIndex = 0x1BA;
+        public const nint m_fAccuracyPenalty = 0x17F0;
+        public const nint m_nBombSite = 0x11A4;
+        public const nint m_bBombDefused = 0x11F4;
+        public const nint m_flDefuseCountDown = 0x11F0;
+        public const nint m_flC4Blow = 0x11D0;
+        public const nint m_bBeingDefused = 0x11DC;
     }
 }
